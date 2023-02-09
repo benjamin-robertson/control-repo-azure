91d913a04f136c9e5241ba4e9dc8476ce33d7f3a
ben <benrobertson9876@gmail.com>
2023-02-09 13:32:43 +1100

    Include office macros

---
+++ site-modules/profile/manifests/office_macros.pp
@@ -0,0 +1,6 @@
+# Class: profile::office_macros
+#
+#
+class profile::office_macros {
+  include acsc_e8_office_hardening
+}
---
+++ site-modules/role/manifests/office_macros.pp
@@ -0,0 +1,6 @@
+# Class: role::office_macros
+#
+#
+class role::office_macros {
+  include profile::office_macros
+}
